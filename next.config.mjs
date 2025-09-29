7bd20abf461ea4cb42c03a328d75975d15a03507
Brian Le <brianle@Brians-MacBook-Pro.local>
2025-09-29 03:25:27 -0700

    edited next.config to fix pathing issue for image loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'cs180-project-2'
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +12,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  output: 'export',
+  trailingSlash: true,
 }
 
 export default nextConfig
